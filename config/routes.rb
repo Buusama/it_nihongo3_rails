f2011afdccbc7d1b7c35111355ed79fa9f06ff6a
Buusama <Buusama1144@gmail.com>
2023-01-30 05:39:51 +0000

    add login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get 'static_pages/home'
   resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+root 'static_pages#home'
 end
